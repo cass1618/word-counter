5e4aea925ed83f0b2ed020561c6ad4b2c63bcc04
cass1618 <cass16180@gmail.com>
2021-03-30 20:33:39 -0700

    Add function to remove words inputted by the user

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
-// Business Logic
-
-function wordCounter(text) {
-  if (text.trim().length === 0) {
-    return 0;
+// Utility Logic
+function sentenceToArray(sentence) {
+  if (sentence.trim().length === 0) {
+    return [];
   }
+  else {
+  return (sentence.split(" "));
+  }
+}
+
+// Business Logic
+function wordCounter(sentence) {
+  const wordArray = sentenceToArray(sentence)
   let wordCount = 0;
-  const wordArray = text.split(" ");
   wordArray.forEach(function(element) {
     if (!Number(element)) {
       wordCount++;
@@ -14,30 +20,44 @@ function wordCounter(text) {
   return wordCount;
 }
 
-function removeBadWords(text) {
-  if (text.trim().length === 0) {
-    return 0;
-  }
-  const wordArray = text.split(" ");
-  let index = 0;
-  wordArray.forEach(function(element) {
+function removeUserBadWords(sentenceString, badString) {
+  const wordArray = sentenceToArray(sentenceString)
+  const badArray = sentenceToArray(badString)
+  const goodArray = ["one"];
 
-    if (element === "muppeteer") {
-      wordArray.splice(index,1);
+  //outer loop: iterates through all of the  sentence words
+  wordArray.forEach(function(word) {
+    //inner loop: iterates through each bad word and compares to the word from the outer loop
+    badArray.forEach(function(badWord) {
       
-    }
-    index ++;
-      
+        if (word != badWord) {
+          goodArray.push(word);
+          console.log("pushed "+word+" to goodArray");
+        }
   });
-
-  console.log(wordArray);
+  // Joins the good word array into a sentence
+  let goodSentence = goodArray.join(" ");
+  console.log(goodSentence);
+  return goodSentence;
+});
 }
 
-function numberOfOccurrencesInText(word, text) {
-  if ((text.trim().length === 0) || (word.trim().length === 0)) {
-    return 0;
-  }
-  const wordArray = text.split(" ");
+function removeBadWords(sentence) {
+  const wordArray = sentenceToArray(sentence)
+  const goodArray = [];
+  // Places all the !bad words in goodArray
+  wordArray.forEach(function(word) {
+    if ((word != "muppeteer")&&(word != "lame")&&(word != "dummy")&&(word != "whatever")) {
+      goodArray.push(word); 
+    }      
+  });
+  // Joins the good words into a sentence
+  let goodSentance = goodArray.join(" ");
+  return goodSentance;
+}
+
+function numberOfOccurrencesInText(word, sentence) {
+  const wordArray = sentenceToArray(sentence));
   let wordCount = 0;
   wordArray.forEach(function(element) {
     if (element.toLowerCase().includes(word.toLowerCase())) {
@@ -47,8 +67,7 @@ function numberOfOccurrencesInText(word, text) {
   return wordCount;
 }
 
-// UI Logic
-
+//UI Logic
 $(document).ready(function(){
   $("form#word-counter").submit(function(event){
     event.preventDefault();
